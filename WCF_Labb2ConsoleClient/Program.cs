9c73a35156632d8c6eb662de507e9d718d6cdb44
Bjorsch <johanbjork47@hotmail.com>
2016-01-19 12:42:42 +0100

    Console BMI done :smile:

--- WCF_Labb2ConsoleClient/Program.cs
+++ WCF_Labb2ConsoleClient/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace WCF_Labb2ConsoleClient
 {
@@ -10,8 +12,13 @@ namespace WCF_Labb2ConsoleClient
     {
         static void Main(string[] args)
         {
-
-
+            BMIClient host = new BMIClient();
+            Console.WriteLine();
+            Console.WriteLine("Skriv in din längd: ");
+            var height = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Skriv in din vikt: ");
+            var weight = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(host.BMI(height, weight));
         }
     }
 }
---
+++ WCF_Labb2WebClient/BMI.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BMI.aspx.cs" Inherits="WCF_Labb2WebClient.BMI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
