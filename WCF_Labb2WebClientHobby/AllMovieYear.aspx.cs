8524aac5c8ad638552550b414cfc0c9f78602e12
Bjorsch <johanbjork47@hotmail.com>
2016-01-21 09:40:58 +0100

    fixat lite

--- WCF_Labb2WebClientHobby/AllMovieYear.aspx.cs
+++ WCF_Labb2WebClientHobby/AllMovieYear.aspx.cs
@@ -9,32 +9,33 @@ namespace WCF_Labb2WebClientHobby
 {
     public partial class AllMovieYear : System.Web.UI.Page
     {
+        private HobbyClient host;
+        public AllMovieYear()
+        {
+            host = new HobbyClient();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
         protected void Button1_Click(object sender, EventArgs e)
-        {
-            HobbyClient host = new HobbyClient();
+        {            
             TextBox1.Text = host.ButtonFromYear("2012");
         }
 
         protected void Button2_Click(object sender, EventArgs e)
-        {
-            HobbyClient host = new HobbyClient();
+        {           
             TextBox1.Text = host.ButtonFromYear("2013");
         }
 
         protected void Button3_Click(object sender, EventArgs e)
-        {
-            HobbyClient host = new HobbyClient();
+        {           
             TextBox1.Text = host.ButtonFromYear("2014");
         }
 
         protected void Button4_Click(object sender, EventArgs e)
-        {
-            HobbyClient host = new HobbyClient();
+        {          
             TextBox1.Text = host.ButtonFromYear("2015");
         }
     }
